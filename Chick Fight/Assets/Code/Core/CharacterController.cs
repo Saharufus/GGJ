43b419f33eafcada6237eea38bf2601f89775c79
DanielMor-Wako <83401330+DanielMor-Wako@users.noreply.github.com>
2024-01-23 14:23:31 +0200

    init and character prefab, and material

---
+++ Chick Fight/Assets/Code/Core/CharacterController.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+
+namespace Code.Core {
+
+    public class CharacterController : MonoBehaviour {
+
+        [SerializeField] private KeyCode leftInput;
+        [SerializeField] private KeyCode rightInput;
+
+        [SerializeField] private Rigidbody2D _rb;
+
+        [SerializeField] private float _rotationSpeed = 100f;
+        [SerializeField] private float _jumpForce = 5f;
+        [SerializeField] private float _powerDuration = 5f;
+
+        private bool _isAlive;
+        private bool _isGrounded;
+        private bool _hasPower;
+        private float _powerTimer;
+
+        private void Awake() {
+
+            _rb ??= GetComponent<Rigidbody2D>();
+        }
+
+        private void Update() {
+
+            float rotationInput = GetInputRotation();
+
+            float rotationAmount = rotationInput * _rotationSpeed * Time.deltaTime;
+
+            _rb.MoveRotation(_rb.rotation + rotationAmount);
+
+            if (_isGrounded) {
+                Jump();
+                _isGrounded = false;
+            }
+
+            UpdatePowerUp();
+        }
+
+        private void FixedUpdate() {
+
+            if (_isAlive) {
+                CheckGrounded();
+            }
+        }
+
+        private float GetInputRotation() {
+            return Input.GetKey(leftInput) ? -1 : Input.GetKey(rightInput) ? 1 : 0f;
+        }
+
+        private void UpdatePowerUp() {
+
+            if (!_hasPower) {
+                return;
+            }
+
+            _powerTimer -= Time.deltaTime;
+            if (_powerTimer <= 0) {
+                EndPower();
+            }
+        }
+
+        private void Jump() {
+
+            Vector2 jumpVelocity = Quaternion.Euler(0f, 0f, _rb.rotation) * Vector2.up * _jumpForce;
+            _rb.velocity = jumpVelocity;
+        }
+
+        private void CheckGrounded() {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
+            _isGrounded = hit.collider != null;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision) {
+
+            // Check if the character collides with the power up item
+            // PickUpPower();
+        }
+
+        private void PickUpPower() {
+
+            _hasPower = true;
+            // todo: get the duration of the powerup from the powerup item or by the gameplay controller
+            _powerTimer = _powerDuration;
+        }
+
+        private void EndPower() {
+
+            _hasPower = false;
+        }
+
+    }
+}
